92d52276540382395b1d6722d8a93bbcf16d6db6
car eat fish <18434367522@163.com>
2019-01-16 19:22:44 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,7 @@ function readFileList(path, filesList) {
   files.forEach(function (itm, index) {
     var stat = fs.statSync(path + itm);
     if (stat.isDirectory()) {
-    //递归读取文件
+      //递归读取文件
       readFileList(path + itm + "/", filesList)
     } else {
       var obj = {};//定义一个对象存放文件的路径和名字
@@ -83,11 +83,62 @@ var getFiles = {
 // console.log(getFiles.getFileList("./upload/"));
 
 
+// fs.rmdir('./upload/notice_news/20190115',function(err){
+//   if(err){
+//       throw err;
+//     console.log("ss");
+//   }else{
+//     console.log("删除了")
+//   }
+// })
+
+
+// 含删除空文件夹功能
+function mydelnullfile(path, filesList) {
+  var files = fs.readdirSync(path);
+  files.forEach(function (itm, index) {
+    var stat = fs.statSync(path + itm);
+    fs.readdir(path + itm,function(err, filess){
+        if (stat.isDirectory()) {
+          if(filess.length === 0){
+            fs.rmdir(path + itm,function(err){
+              if (err) {
+                  return console.error(err);
+              }
+            })
+          }
+          mydelnullfile(path + itm + "/", filesList)
+        } 
+    })
+  })
+}
+function getFileList (path) {
+  var filesList = [];
+  mydelnullfile(path, filesList);
+  return filesList;
+};
+
+// getFileList("./upload/");
 
 
 
 
 
+// function delNullFile(path){
+//   fs.readdir(path,function(err, files){
+//     if (err) {
+//         return console.error(err);
+//     }
+//     if(files.length === 0){
+//       console.log("空文件")
+//   // fs.rmdir(path, callback)
+//     }else{
+//       console.log("有文件")
+//     }
+//   });
+// }
+
+// console.log(delNullFile('./upload/notice_news/20190115'))
 
 
 
@@ -127,6 +178,60 @@ router.get('/api/', function(req, res, next) {
   req.session.lastPage = '/awesome'; //每一次访问时，session对象的lastPage会自动的保存或更新内存中的session中去。
   res.send("You're Awesome. And the session expired time is: " + req.session.cookie.maxAge);
 });
+/* ==================================================================  数据库管理  =============================================== */
+router.post('/api/database/delnullfile',function(req,res){
+  getFileList("./upload/");
+  res.json({status:200,message:"空文件夹清理成功"});
+})
+router.post('/api/database/notice_news',function(req,res){
+  pool.query('select thumb from notice_news', function(err, rows, fields) {
+    if (err){throw err;} 
+    var a = [];
+    var b = [];
+    rows.map((item,index)=>{
+      a.push(item.thumb);
+    })
+    var imgurl = getFiles.getFileList("./upload/notice_news/");
+    imgurl.map((item,index)=>{
+      b.push(item.path+item.filename);
+    });
+    // 并集
+    var union = a.concat(b.filter(function(v) {return a.indexOf(v) === -1})) 
+    // 交集
+    var intersection = a.filter(function(v){ return b.indexOf(v) > -1 }) 
+    // 差集
+    var difference = a.filter(function(v){ return b.indexOf(v) === -1 })
+    res.json({status:200,data:{allimg:union,nowuseimg:intersection}})
+  });
+})
+router.post('/api/database/delnotice_news',function(req,res){
+  pool.query('select thumb from notice_news', function(err, rows, fields) {
+    if (err){throw err;} 
+    var a = [];
+    var b = [];
+    rows.map((item,index)=>{
+      b.push(item.thumb);
+    })
+    var imgurl = getFiles.getFileList("./upload/notice_news/");
+    imgurl.map((item,index)=>{
+      a.push(item.path+item.filename);
+    });
+    //并集
+    var union = a.concat(b.filter(function(v) {return a.indexOf(v) === -1})) 
+    // 交集
+    var intersection = a.filter(function(v){ return b.indexOf(v) > -1 }) 
+    // 差集
+    var difference = a.filter(function(v){ return b.indexOf(v) === -1 })
+    
+    difference.map((item,index)=>{
+      fs.unlink(item, function(err){{
+        if (err) {return console.error(err);}
+      }})
+    })
+    res.json({status:200,message:"删除成功",data:{chaji:difference}})
+  });
+})
+
 
 /* ==================================================================  管理员管理  =============================================== */
 // 添加管理员信息
@@ -242,7 +347,7 @@ router.post('/api/notice_news',function(req,res,next){
       item.status = item.status == 1 ? "草稿" : item.status == 2 ? "等待审核" : item.status == 3 ? "已发布" : "未知状态";
       item.ontop = item.ontop == 1 ? "置顶" : "不置顶";
       item.iselite = item.iselite == 1 ? "推荐" : "不推荐";
-      item.add_time = formateDate(item.add_time,"Y-M-D h:min:s")
+      item.add_time = formateDate(new Date(item.add_time),"Y-M-D h:min:s")
     })
     res.send(rows)
   });
@@ -279,22 +384,28 @@ router.post('/api/addnotice_news', function(req, res) {
               console.log('rename error: ' + err);
               res.JSON({status:400, message: '上传失败！'});
             } else {
-              console.log(fields)
-              var name = fields.name ? fields.name : -1;
-              var sort = fields.sort ? fields.sort : -1;
-              var type = fields.type ? fields.type : -1;
-              var link = fields.link ? fields.link : -1;
+              var sort = fields.sort,
+                  pid = fields.pid,
+                  author = fields.author,
+                  status = fields.status,
+                  title = fields.title,
+                  keyword = fields.keyword,
+                  content = fields.content,
+                  ontop = fields.ontop,
+                  iselite = fields.iselite,
+                  add_time = fields.add_time,
+                  thumb = dstPath;
               console.log('rename ok'); 
-              // pool.query(`INSERT INTO notice_news (name,type,sort,link,img) VALUES ('${name}','${type}','${sort}','${link}','${dstPath}')`,function(error, rows, fields){
-              //   if(error){
-              //     res.json({status:399, message: '添加失败！'});
-              //   }
-              //   if(rows.affectedRows !== 1){
-              //     res.json({status:400, message: '添加失败！'});
-              //   }else {
-              //     res.json({status:200, message: '添加成功！'});
-              //   }
-              // })
+              pool.query(`INSERT INTO notice_news (sort,pid,author,status,title,keyword,content,ontop,iselite,add_time,thumb) VALUES ('${sort}','${pid}','${author}','${status}','${title}','${keyword}','${content}','${ontop}','${iselite}','${add_time}','${thumb}')`,function(error, rows, fields){
+                if(error){
+                  res.json({status:399, message: '添加失败！'});
+                }
+                if(rows.affectedRows !== 1){
+                  res.json({status:400, message: '添加失败！'});
+                }else {
+                  res.json({status:200, message: '添加成功！'});
+                }
+              })
             }
           });
         }
@@ -309,7 +420,91 @@ router.post('/api/addnotice_news', function(req, res) {
     }
   });
 });
-
+// 修改信息
+router.post('/api/editnotice_news', function(req, res) {
+  var form = new multiparty.Form();
+  form.encoding = 'utf-8';        
+  var ndate = new Date();
+  var nstamp = ndate.getTime();
+  var Y = ndate.getFullYear();
+  var M = Number(ndate.getMonth())+1 < 10 ? "0"+ Number(ndate.getMonth()+1) : ndate.getMonth()+1;
+  var D = ndate.getDate() < 10 ? "0"+ ndate.getDate() : ndate.getDate(); 
+  var folderName = String(Y)+String(M)+String(D);   
+  function doupload () {
+      form.uploadDir = `upload/notice_news/${folderName}`;         //设置文件存储路径
+      form.maxFilesSize = 2 * 1024 * 1024;        //设置单文件大小限制
+      //form.maxFields = 1000;  设置所以文件的大小总和    对于多文件
+      //上传完成后处理
+      form.parse(req, function(err, fields, files) {
+        var obj ={};
+        var filesTmp = JSON.stringify(files,null,2);
+        if(err){
+          console.log('parse error: ' + err);
+          res.json({status:400, message: '上传失败！'});
+        }else{
+          var inputFile = files.inputFile[0];
+          var uploadedPath = inputFile.path;
+          var fileFormat = (inputFile.originalFilename).split(".")[1];
+          var dstPath = `./upload/notice_news/${folderName}/` + nstamp + "." + fileFormat;
+          //重命名为真实文件名
+          fs.rename(uploadedPath, dstPath, function(err) {
+            if(err){
+              console.log('rename error: ' + err);
+              res.JSON({status:400, message: '上传失败！'});
+            } else {
+              var sort = Number(fields.sort),
+                  id = Number(fields.id),
+                  pid = Number(fields.pid),
+                  author = fields.author,
+                  status = Number(fields.status),
+                  title = fields.title,
+                  keyword = fields.keyword,
+                  content = fields.content,
+                  ontop = Number(fields.ontop),
+                  iselite = Number(fields.iselite),
+                  add_time = fields.add_time,
+                  thumb = dstPath;
+                  console.log(sort,id,pid,author,status,title,keyword,content,ontop,iselite,add_time,thumb )
+              console.log('rename ok'); 
+              
+              pool.query(`UPDATE notice_news SET sort = ${sort},pid = ${pid},author = '${author}',status = ${status},title = '${title}',keyword = '${keyword}',content = '${content}',ontop = ${ontop},iselite = ${iselite},add_time = '${add_time}',thumb = '${thumb}'  WHERE id = ${id}`,function(error, rows, fields){
+                if(error){
+                  res.json({status:399, message: '更新失败！'});
+                }
+                if(rows.affectedRows !== 1){
+                  res.json({status:400, message: '更新失败！'});
+                }else {
+                  res.json({status:200, message: '更新成功！'});
+                }
+              })
+            }
+          });
+        }
+      });
+  }
+  fs.exists(`upload/notice_news/${folderName}`, function(exists) {
+    if(!exists){
+      fs.mkdirSync(`./upload/notice_news/${folderName}`)
+      doupload ();
+    }else{
+      doupload ();
+    }
+  });
+});
+// 删除广告信息
+router.post('/api/delnotice_news',function(req,res){
+  var id = Number(req.body.id);
+  pool.query(`DELETE FROM notice_news WHERE id=${id}`,function(error, rows, fields){
+    if(error){
+      res.json({status:399, message: '删除失败！'});
+    }
+    if(rows.affectedRows !== 1){
+      res.json({status:400, message: '删除失败！'});
+    }else {
+      res.json({status:200, message: '删除成功！'});
+    }
+  })
+});
 /* ==================================================================  广告管理  =============================================== */
 // 获取广告信息
 router.post('/api/advertisement', function(req, res, next) {
